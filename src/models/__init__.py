575aad1874b8b59760fbb077c52e8cfee3f686b0
Ignorant <2185018561@qq.com>
2026-05-20 19:16:35 +0800

    从零开始重构

--- src/models/__init__.py
+++
@@ -1,8 +0,0 @@
-"""模型包导出入口。
-
-外部代码可以通过 `from src.models import ImprovedBayesianPVNet` 直接导入主模型。
-"""
-
-from src.models.improved_bnn import ImprovedBayesianPVNet
-
-__all__ = ["ImprovedBayesianPVNet"]
